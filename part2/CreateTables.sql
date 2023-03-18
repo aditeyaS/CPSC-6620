d2acdfab5fe0259e0fa0ea767b815a1ab55a1c89
Aditeya Srivastava <aditeya@Aditeyas-MacBook-Air.local>
2023-03-17 20:16:52 -0400

    added orders

--- part2/CreateTables.sql
+++ part2/CreateTables.sql
@@ -4,8 +4,8 @@
 -- Pizza base table
 -- Contains information on cost of all pizza combination (size x crust)
 CREATE TABLE base (
-    BaseSize VARCHAR(15) NOT NULL,
-    BaseCrust VARCHAR(15) NOT NULL,
+    BaseSize VARCHAR(20) NOT NULL,
+    BaseCrust VARCHAR(20) NOT NULL,
     BaseSP DECIMAL(4, 2) NOT NULL,
     BaseCP DECIMAL(4, 2) NOT NULL,
     PRIMARY KEY (BaseSize, BaseCrust)
@@ -39,7 +39,47 @@ CREATE TABLE customer (
     CustomerID PRIMARY KEY INTEGER NOT NULL AUTO_INCREMENT,
     CustomerFName VARCHAR(25) NOT NULL,
     CustomerLName VARCHAR(25) NOT NULL,
-    CustomerPhone VARCHAR(10) NOT NULL
+    CustomerPhone VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE order (
+    OrderID INTEGER NOT NULL AUTO_INCREMENT,
+    OrderType VARCHAR(15) NOT NULL,
+    OrderTime DATETIME NOT NULL,
+    OrderSP DECIMAL(10, 2) NOT NULL,
+    OrderCP DECIMAL(10, 2) NOT NULL,
+    OrderCustomerID INTEGER FOREIGN KEY REFERENCES customer(CustomerID),
+    PRIMARY KEY(OrderID)
+);
+
+CREATE TABLE order_discount (
+    OrderDiscountOrderID INTEGER NOT NULL FOREIGN KEY REFERENCES order(OrderID),
+    OrderDiscountDiscountID INTEGER NOT NULL FOREIGN KEY REFERENCES discount(DiscountID),
+    PRIMARY KEY (OrderDiscountOrderID, OrderDiscountDiscountID)
+);
+
+CREATE TABLE pizza (
+    PizzaID INTEGER NOT NULL AUTO_INCREMENT,
+    PizzaBaseSize VARCHAR(20) NOT NULL FOREIGN KEY REFERENCES base(BaseSize),
+    PizzaBaseCrust VARCHAR(20) NOT NULL FOREIGN KEY REFERENCES base(BaseCrust),
+    PizzaState VARCHAR(20) NOT NULL,
+    PizzaSP DECIMAL(10, 2) NOT NULL,
+    PizzaCP DECIMAL(10, 2) NOT NULL,
+    PizzaOrderID INTEGER NOT NULL FOREIGN KEY REFERENCES order(OrderID),
+    PRIMARY KEY (PizzaID)
+);
+
+CREATE TABLE pizza_topping (
+    PizzaToppingPizzaID INTEGER NOT NULL FOREIGN KEY REFERENCES pizza(PizzaID),
+    PizzaToppingToppingID INTEGER NOT NULL FOREIGN KEY REFERENCES topping(ToppingID),
+    PizzaToppingCount INTEGER NOT NULL,
+    PRIMARY KEY(PizzaToppingPizzaID, PizzaToppingToppingID)
+);
+
+CREATE TABLE pizza_discount (
+    PizzaDiscountPizzaID INTEGER NOT NULL FOREIGN KEY REFERENCES pizza(PizzaID),
+    PizzaDiscountDiscountID INTEGER NOT NULL FOREIGN KEY REFERENCES discount(DiscountID),
+    PRIMARY KEY (PizzaDiscountPizzaID, PizzaDiscountDiscountID)
 );
 
 
---
+++ part2/pizza_orders.sql
@@ -0,0 +1,47 @@
+-- On March 5th at 12:03 pm there was a dine-in order for a large thin crust pizza with Regular Cheese
+-- (extra), Pepperoni, and Sausage (Price: 13.50, Cost: 3.68 ). They used the “Lunch Special Large” discount
+-- They sat at Table 14
+INSERT INTO order ()
+
+-- On March 3rd at 12:05 pm there was a dine-in order At table 4. They ordered a medium pan pizza with
+-- Feta Cheese, Black Olives, Roma Tomatoes, Mushrooms and Banana Peppers (P: 10.60, C: 3.23). They get
+-- the “Lunch Special Medium”and the “Specialty Pizza” discounts. They also ordered a small original crust
+-- pizza with Regular Cheese, Chicken and Banana Peppers (P: 6.75, C: 1.40)
+
+
+-- On March 3rd at 9:30 pm Ellis Beck places an order for pickup of 6 large original crust pizzas with Regular
+-- Cheese and Pepperoni (Price: 10.75, Cost:3.30 each). Ellis’ phone number is 864-254-5861.
+INSERT INTO customer (CustomerFName, CustomerLName, CustomerPhone)
+VALUES ('Ellis', 'Beck', '864-254-5861');
+
+
+-- On March 5th at 7:11 pm there was a delivery order made by Ellis Beck for 1 x-large pepperoni and
+-- Sausage pizza (P 14.50, C 5.59), one x-large pizza with Ham (extra) and Pineapple (extra) pizza (P: 17, C:
+-- 5.59), and one x-large Jalapeno and Bacon pizza (P: 14.00, C: 5.68). All the pizzas have the Four Cheese
+-- Blend on it and are original crust. The order has the “Gameday Special” discount applied to it, and the
+-- ham and pineapple pizza has the “Specialty Pizza” discount applied to it. The pizzas were delivered to 115
+-- Party Blvd, Anderson SC 29621. His phone number is the same as before.
+
+
+-- On March 2nd at 5:30 pm Kurt McKinney placed an order for pickup for an x-large pizza with Green Pepper,
+-- Onion, Roma Tomatoes, Mushrooms, and Black Olives on it. He wants the Goat Cheese on it, and a Gluten
+-- Free Crust (P: 16.85, C:7.85). The “Specialty Pizza” discount is applied to the pizza. Kurt’s phone number is
+-- 864-474-9953.
+INSERT INTO customer (CustomerFName, CustomerLName, CustomerPhone)
+VALUES ('Kurt', 'McKinney', '864-474-9953');
+
+
+-- On March 2nd at 6:17 pm Calvin Sanders places on order for delivery of one large pizza with Chicken,
+-- Green Peppers, Onions, and Mushrooms. He wants the Four Cheese Blend (extra) and thin crust (P:
+-- 13.25, C: 3.20). The pizza was delivered to 6745 Wessex St Anderson SC 29621. Calvin’s phone number is
+-- 864-232-8944.
+INSERT INTO customer (CustomerFName, CustomerLName, CustomerPhone)
+VALUES ('Calvin', 'Sanders', '864-232-8944');
+
+
+-- On March 6th at 8:32 pm Lance Benton ordered two large thin crust pizzas. One had the Four Cheese
+-- Blend on it (extra) (P: 12, C: 3.75), the other was Regular Cheese and Pepperoni (extra) (P:12, C: 2.55). He
+-- used the “Employee” discount on his order. He had them delivered to 8879 Suburban Home, Anderson,
+-- SC 29621. His phone number is 864-878-5679.
+INSERT INTO customer (CustomerFName, CustomerLName, CustomerPhone)
+VALUES ('Lance', 'Benton', '864-232-8944');
